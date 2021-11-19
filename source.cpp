8fe0dca3bb60f6b827383903e10d98178d8f86e0
levinh0612 <72688487+levinh0612@users.noreply.github.com>
2021-11-19 15:09:28 +0700

    v

--- source.cpp
+++ source.cpp
@@ -8,22 +8,12 @@ int hieu(int a, int b) {
 	return a-b;
 };
 // Da xoa tinh tong cua user2
-<<<<<<< HEAD
-=======
+
 // tach thanh nhanh tich by:user1
 int tich(int a, int b) {
 	return a*b;
 }
-//Ktra chan le by: User2
-void kiemTraChanLe(int n) {
-	if(n%2==0) {
-		cout << n <<"la so chan";
-	} else {
-		cout <<n <<"la so le ";
-	}
-}
-//end Chan le
->>>>>>> eb12d4e4e4ed4d11436760c274f6caab8d1febb9
+
 int main()
 {
 	int a, b;
